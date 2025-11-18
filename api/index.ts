42b5cf368e737b6198694af31c1bd0db109da505
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2025-11-18 20:32:35 -0300

    teste

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import { fastify } from "fastify" 
+import cors from "@fastify/cors"
+
+
+
+const app = fastify ();
+
+app.register( cors, {
+    origin: "*"
+})
+
+app.get('/', () =>{
+    return 'ola mundo'
+})
